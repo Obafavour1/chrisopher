8e6a7b690da03b4efaf664f7bdc176d78e8411e2
Obafavour1 <olobafavour@gmail.com>
2026-02-27 18:08:25 +0100

    google translate

--- components/languageTranslate/GoogleTranslate.tsx
+++ components/languageTranslate/GoogleTranslate.tsx
@@ -52,7 +52,8 @@ export default function GoogleTranslate() {
   return (
     <div
       id="google_translate_element"
-      style={{ visibility: "hidden", position: "absolute", top: "-9999px" }}
+      style={{ display: "none" }}
     />
   );
 }
+
